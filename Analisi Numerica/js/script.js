f12870711068f464a0e283974e2a8bbe7741fc31
Samuele Dimatteo <samuele.dimatteo@jcmaxwell.it>
2022-04-29 00:11:37 +0200

    Analisi Numerica
    Analisi Numerica Fatta Su 6 Esercizi

---
+++ Analisi Numerica/js/script.js
@@ -0,0 +1,345 @@
+$("body").ready(function(){
+  $(".titile-es1").css("display", "none");
+  $(".iniziorispostaes1").css("display", "none");
+  $(".titile-es2").css("display", "none");
+  $(".iniziorispostaes2").css("display", "none");
+  $(".titile-es3").css("display", "none");
+  $(".iniziorispostaes3").css("display", "none");
+  $(".titile-es4").css("display", "none");
+  $(".iniziorispostaes4").css("display", "none");
+  $(".titile-es5").css("display", "none");
+  $(".iniziorispostaes5").css("display", "none");
+  $(".titile-es6").css("display", "none");
+  $(".iniziorispostaes6").css("display", "none");
+  $(".back").css("display", "none");
+});
+
+$(".back").click(function(){
+  $(".back").css("display", "none");
+  $(".btn-es1").css("display", "block"); 
+  $(".titile-es1").css("display", "none");
+  $(".iniziorispostaes1").css("display", "none");
+  $(".btn-es2").css("display", "block"); 
+  $(".titile-es2").css("display", "none");
+  $(".iniziorispostaes2").css("display", "none");
+  $(".btn-es3").css("display", "block"); 
+  $(".titile-es3").css("display", "none");
+  $(".iniziorispostaes3").css("display", "none");
+  $(".btn-es4").css("display", "block"); 
+  $(".titile-es4").css("display", "none");
+  $(".iniziorispostaes4").css("display", "none");
+  $(".btn-es5").css("display", "block"); 
+  $(".titile-es5").css("display", "none");
+  $(".iniziorispostaes5").css("display", "none");
+  $(".btn-es6").css("display", "block"); 
+  $(".titile-es6").css("display", "none");
+  $(".iniziorispostaes6").css("display", "none");
+});
+
+$(".btn-es1").click(function(){
+  $(".back").css("display", "block");
+  $(".btn-es1").css("display", "none"); 
+  $(".titile-es1").css("display", "block");
+  $(".iniziorispostaes1").css("display", "block");
+  $(".btn-es2").css("display", "none"); 
+  $(".titile-es2").css("display", "none");
+  $(".iniziorispostaes2").css("display", "none");
+  $(".btn-es3").css("display", "none"); 
+  $(".titile-es3").css("display", "none");
+  $(".iniziorispostaes3").css("display", "none");
+  $(".btn-es4").css("display", "none"); 
+  $(".titile-es4").css("display", "none");
+  $(".iniziorispostaes4").css("display", "none");
+  $(".btn-es5").css("display", "none"); 
+  $(".titile-es5").css("display", "none");
+  $(".iniziorispostaes5").css("display", "none");
+  $(".btn-es6").css("display", "none"); 
+  $(".titile-es6").css("display", "none");
+  $(".iniziorispostaes6").css("display", "none");
+});
+
+$(".btn-es2").click(function(){
+  $(".back").css("display", "block");
+  $(".btn-es1").css("display", "none"); 
+  $(".titile-es1").css("display", "none");
+  $(".iniziorispostaes1").css("display", "none");
+  $(".btn-es2").css("display", "none"); 
+  $(".titile-es2").css("display", "block");
+  $(".iniziorispostaes2").css("display", "block");
+  $(".btn-es3").css("display", "none"); 
+  $(".titile-es3").css("display", "none");
+  $(".iniziorispostaes3").css("display", "none");
+  $(".btn-es4").css("display", "none"); 
+  $(".titile-es4").css("display", "none");
+  $(".iniziorispostaes4").css("display", "none");
+  $(".btn-es5").css("display", "none"); 
+  $(".titile-es5").css("display", "none");
+  $(".iniziorispostaes5").css("display", "none");
+  $(".btn-es6").css("display", "none"); 
+  $(".titile-es6").css("display", "none");
+  $(".iniziorispostaes6").css("display", "none");
+});
+
+$(".btn-es3").click(function(){
+  $(".back").css("display", "block");
+  $(".btn-es1").css("display", "none"); 
+  $(".titile-es1").css("display", "none");
+  $(".iniziorispostaes1").css("display", "none");
+  $(".btn-es2").css("display", "none"); 
+  $(".titile-es2").css("display", "none");
+  $(".iniziorispostaes2").css("display", "none");
+  $(".btn-es3").css("display", "none"); 
+  $(".titile-es3").css("display", "block");
+  $(".iniziorispostaes3").css("display", "block");
+  $(".btn-es4").css("display", "none"); 
+  $(".titile-es4").css("display", "none");
+  $(".iniziorispostaes4").css("display", "none");
+  $(".btn-es5").css("display", "none"); 
+  $(".titile-es5").css("display", "none");
+  $(".iniziorispostaes5").css("display", "none");
+  $(".btn-es6").css("display", "none"); 
+  $(".titile-es6").css("display", "none");
+  $(".iniziorispostaes6").css("display", "none");
+});
+
+$(".btn-es4").click(function(){
+  $(".back").css("display", "block");
+  $(".btn-es1").css("display", "none"); 
+  $(".titile-es1").css("display", "none");
+  $(".iniziorispostaes1").css("display", "none");
+  $(".btn-es2").css("display", "none"); 
+  $(".titile-es2").css("display", "none");
+  $(".iniziorispostaes2").css("display", "none");
+  $(".btn-es3").css("display", "none"); 
+  $(".titile-es3").css("display", "none");
+  $(".iniziorispostaes3").css("display", "none");
+  $(".btn-es4").css("display", "none"); 
+  $(".titile-es4").css("display", "block");
+  $(".iniziorispostaes4").css("display", "block");
+  $(".btn-es5").css("display", "none"); 
+  $(".titile-es5").css("display", "none");
+  $(".iniziorispostaes5").css("display", "none");
+  $(".btn-es6").css("display", "none"); 
+  $(".titile-es6").css("display", "none");
+  $(".iniziorispostaes6").css("display", "none");
+});
+
+$(".btn-es5").click(function(){
+  $(".back").css("display", "block");
+  $(".btn-es1").css("display", "none"); 
+  $(".titile-es1").css("display", "none");
+  $(".iniziorispostaes1").css("display", "none");
+  $(".btn-es2").css("display", "none"); 
+  $(".titile-es2").css("display", "none");
+  $(".iniziorispostaes2").css("display", "none");
+  $(".btn-es3").css("display", "none"); 
+  $(".titile-es3").css("display", "none");
+  $(".iniziorispostaes3").css("display", "none");
+  $(".btn-es4").css("display", "none"); 
+  $(".titile-es4").css("display", "none");
+  $(".iniziorispostaes4").css("display", "none");
+  $(".btn-es5").css("display", "none"); 
+  $(".titile-es5").css("display", "block");
+  $(".iniziorispostaes5").css("display", "block");
+  $(".btn-es6").css("display", "none"); 
+  $(".titile-es6").css("display", "none");
+  $(".iniziorispostaes6").css("display", "none");
+});
+
+$(".btn-es6").click(function(){
+  $(".back").css("display", "block");
+  $(".btn-es1").css("display", "none"); 
+  $(".titile-es1").css("display", "none");
+  $(".iniziorispostaes1").css("display", "none");
+  $(".btn-es2").css("display", "none"); 
+  $(".titile-es2").css("display", "none");
+  $(".iniziorispostaes2").css("display", "none");
+  $(".btn-es3").css("display", "none"); 
+  $(".titile-es3").css("display", "none");
+  $(".iniziorispostaes3").css("display", "none");
+  $(".btn-es4").css("display", "none"); 
+  $(".titile-es4").css("display", "none");
+  $(".iniziorispostaes4").css("display", "none");
+  $(".btn-es5").css("display", "none"); 
+  $(".titile-es5").css("display", "none");
+  $(".iniziorispostaes5").css("display", "none");
+  $(".btn-es6").css("display", "none"); 
+  $(".titile-es6").css("display", "block");
+  $(".iniziorispostaes6").css("display", "block");
+});
+
+function EsercizioN1() {
+
+var factor = 1;
+var countfactors = 0;
+var numberfactorsmax;
+var factorsmax = 0;
+
+    for(var num = 1; num<1000; num++) {
+
+        for(factor = 1; factor<=num/2; factor++) {
+
+            if(num%factor == 0) {
+                    
+                countfactors++;
+
+                if(countfactors>factorsmax) {
+
+                    factorsmax = countfactors + 1;
+
+                    numberfactorsmax = num;
+
+                    document.getElementById("rispostaes1").innerHTML = numberfactorsmax;
+                }
+            }
+        }
+
+        countfactors = 0;
+
+    }
+
+    return numberfactorsmax;
+           
+}
+
+function EsercizioN2() {
+
+var factor=1;
+var countfactors2=1;
+var sumfactor=0;
+
+    for(let num2 = 1; num2<1000; num2++) {
+
+        for(factor = 1; factor<=num2/2; factor++) {
+
+            if(num2%factor == 0) {
+
+                countfactors2++;
+            }
+        }
+
+            if(countfactors2>2) {
+
+            sumfactor = sumfactor + countfactors2;
+
+            document.getElementById("rispostaes2").innerHTML = sumfactor;
+        } 
+
+        countfactors2 = 0; 
+    }
+        
+    return sumfactor;
+
+}
+
+function EsercizioN3() {
+
+var factor3 = 1;
+var countfactors3 = 0;
+var numberfactorsmax3;
+var factorsmax3 = 0;
+
+    for(var num3 = 1000; num3<2000; num3++) {
+
+        for(factor3 = 1; factor3<=num3/2; factor3++) {
+
+            if(num3%factor3 == 0) {
+                    
+                countfactors3++;
+
+                if(countfactors3>factorsmax3) {
+
+                    factorsmax3 = countfactors3 + 1;
+
+                    numberfactorsmax3 = num3;
+
+                    document.getElementById("rispostaes3").innerHTML = EsercizioN1() * numberfactorsmax3;
+                }
+            }
+        }
+
+        countfactors3 = 0;
+
+    }
+
+    return numberfactorsmax3;
+           
+}
+
+function EsercizioN4(){
+
+var factor4 = 1;
+var countfactors4 = 1;
+var num4 = EsercizioN1();
+
+   do{
+
+        countfactors4 = 1;
+
+        num4++;
+
+            for(factor4 = 1; factor4<=num4/2; factor4++) {
+
+            if(num4%factor4 == 0) {
+
+                countfactors4++;
+
+                document.getElementById("rispostaes4").innerHTML = num4;
+            }
+        }   
+    }
+
+    while(countfactors4>2);
+
+    return num4;
+}
+
+function EsercizioN5(){
+
+var factor5 = 1;
+var countfactors5= 1;
+var num5 = EsercizioN2();
+
+    do {
+
+        countfactors5 = 1;
+
+        num5++;
+
+        for(factor5=1;factor5<=num5/2;factor5++) {
+
+        if(num5%factor5 == 0) {
+
+                countfactors5++;
+                
+                document.getElementById("rispostaes5").innerHTML = num5;
+            }
+        }   
+    }
+
+    while(countfactors5>2);
+
+    return num5;
+}
+
+
+function EsercizioN6(){
+
+var factor6 = 1;
+var factorout = "";
+var countfactors6 = 1;
+
+    var num6 = EsercizioN4() * EsercizioN5();
+
+    for(factor6 = 1; factor6<=num6/2; factor6++) {
+
+            if(num6%factor6 == 0) {
+
+                factorout = factorout + factor6 +" ";
+
+                countfactors6++;
+
+                document.getElementById("rispostaes6").innerHTML = num6;
+            }
+        }
+    }     
